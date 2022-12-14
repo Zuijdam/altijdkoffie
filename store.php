0eabdf27782760d2883e1a7be981c0ea34a86ec6
Erik van Zuijdam <35580176+Zuijdam@users.noreply.github.com>
2022-12-14 21:08:36 +0100

    Zo

--- store.php
+++ store.php
@@ -2,7 +2,7 @@
 //R::debug(false);
 // includes
 require 'include/rb-mysql.php';
-include config.php;
+include 'config.php';
 
 // db connectie
 R::setup(
@@ -11,9 +11,14 @@ R::setup(
     $DatabasePassword
   );
 
+// opslaan van waarde uit de querystring in een variabele
+
+$gewicht = $_GET['gewicht'];
+
 //opslaan waarde van weeschaal in custom DB in tabel weging
 $weging = R::dispense( 'weging' );
-$weging->gewicht = 1;
+$weging->gewicht = $gewicht;
+$weging->datum = date("Y-m-d", strtotime("-1 days"));
 $id = R::store( $weging );
 
 ?>
