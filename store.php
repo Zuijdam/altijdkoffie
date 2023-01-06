ad1258cb0cd5d53119809ac08ae23cbaa1c838b8
Erik van Zuijdam <35580176+Zuijdam@users.noreply.github.com>
2023-01-06 11:54:10 +0100

    type store toegevoegd

--- store.php
+++ store.php
@@ -14,15 +14,24 @@ R::setup(
 // opslaan van waarde uit de querystring in een variabele
 
 $gewicht = $_GET['gewicht'];
+$type = $_GET['type'];
 
 //opslaan waarde van weeschaal in custom DB in tabel weging
 if($_GET['key']  != $SuperGeheimeKey) exit();
 
+if (isset($_GET['type']) && $_GET['type'] == "meting") {
 $weging = R::dispense( 'weging' );
 $weging->gewicht = $gewicht;
 $weging->datum = date("Y-m-d H:i:s");
 $id = R::store( $weging );
+}
 
+if (isset($_GET['type']) && $_GET['type'] == "tare") {
+$weging = R::dispense( 'weging' );
+$weging->tare = $gewicht;
+$weging->datum = date("Y-m-d H:i:s");
+$id = R::store( $weging );
+}
 
 // laatste waarde laten zien op het scherm
 $weging= R::load('weging',$id);
