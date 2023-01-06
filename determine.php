8c9e348cb1f802abf0ae3c91b9be87ec2901928f
Erik van Zuijdam <35580176+Zuijdam@users.noreply.github.com>
2023-01-06 15:17:22 +0100

    Update determine.php

--- determine.php
+++ determine.php
@@ -10,30 +10,17 @@ R::setup(
     $DatabasePassword
   );
 
-  // laten zien laatste 10 wegingen
+ // Pak alle metingen van de afgelopen paar minuten en berekend het gemm.
+// Bepaal de laaste Tare value
+// Bepaal het verschil tussen het gemm. en de tare
+// Als dat hoger is dan 50% dan geef een melding 'er is koffie nodig!!!'
 
-$wegingen = R::findAll('weging','ORDER BY id DESC LIMIT 10');
+$wegingen = R::findAll('weging','ORDER BY id DESC LIMIT 30');
 
 
 echo "<pre>";
 foreach ($wegingen as $value) {
   echo $value . "<br>";
-}
-echo "</pre>";
-
-
-//
-// Pak alle metingen van de afgelopen 30 minuten en berekend het gemm.
-// Bepaal de laaste Tare value
-// Bepaal het verschil tussen het gemm. en de tare
-// Als dat hoger is dan 50% dan geef een melding 'er is koffie nodig!!!'
-
-$tijd = time() - 1800; // tijd 30 minuten geleden
-$waarden = R::find('weging', 'datum > ?', [$tijd]);
-
-echo "<pre>";
-foreach($waarden as $waarde) {
-    echo $waarde . "<br>";
 }
 echo "</pre>";
 
